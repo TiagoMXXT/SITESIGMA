1295932cb6cfc1e454284a9281907c21c999936d
TiagoMXXT <93770432+TiagoMXXT@users.noreply.github.com>
2022-08-25 10:42:11 -0300

    Backup
    25/08/2022

---
+++ produto-detalhe.php
@@ -0,0 +1,30 @@
+<?php
+    // include do footer
+    include_once './includes/_dados.php';
+    include_once './includes/_head.php';
+    include_once './includes/_header.php';
+
+    $id = $_GET['id'];
+    @$tipo = $_GET['tipo'];
+
+?>
+
+    <div class="container">
+        <div class="row">
+            <div class=""col>
+                <h1> <?php echo $produtos[$id]['nome']; ?> </h1>
+                <p> <?php echo $produtos[$id]['descricao']; ?> </p>
+                <img src="<?php echo $produtos[$id]['imagem']; ?>">
+                <h4>R$ <?php echo $produtos[$id]['preco']; ?></h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">Voltar</div>
+            <div class="col">Comprar</div>
+        </div>
+    </div>
+
+<?php
+    // include do footer
+    include_once './includes/_footer.php';
+?>
